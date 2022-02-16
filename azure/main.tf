10e8db0ac91110c9a788016043c6d2dfdaef7b14
Wesley van Ede <djpollux@msn.com>
2022-02-16 11:38:14 +0100

    updated last tag to 2nd tag

--- azure/main.tf
+++ azure/main.tf
@@ -2,7 +2,7 @@
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you're using version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
+    version = "~>2.1"
     features {}
 }
 
@@ -142,6 +142,6 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     }
 
     tags = {
-        environment = var.myTag
+        environment = var.mySecondTag
     }
 }
